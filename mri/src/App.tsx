088801ca0f413f666b59c0187f803b6d5661c7ec
Nicholas Cammisuli <nicholas.cammisuli@gmail.com>
2025-04-23 20:34:11 -0400

    basic landing page layout using tailwind

--- mri/src/App.tsx
+++ mri/src/App.tsx
@@ -7,13 +7,18 @@ import './App.css'
 function Home(){
   return(
     <>
-      <h1>ACL Ligament Helper</h1>
+    <div className="columns-2">
+      <h1 className='pb-5'>ACL Ligament Helper</h1>
+      <p className='pb-5'>Classify and detect types of ACL tears in MRI images with our algorythms</p>
       <Link to="/upload">
-        <button>
+        <button className='text-white'>
           Get Started
         </button>
       </Link>
-      
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTr_mGmSutVv2ynXuPwalU_8QOGiOCqoMOQzA&s" alt="bones"
+          width="500" 
+          height="500" />
+    </div>
     </>
   );
 }
