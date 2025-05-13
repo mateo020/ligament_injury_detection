e7766231654d86069f9685704de66e915e7fe931
Nicholas Cammisuli <nicholas.cammisuli@gmail.com>
2025-05-12 20:05:08 -0400

    Basic layout for the analysis page

--- mri/src/components/Analysis.tsx
+++ mri/src/components/Analysis.tsx
@@ -1,8 +1,30 @@
-
-function Analysis(){
+function Analysis() {
     return (
-       <div>PLACEHOLDER</div>
-    )
+        <div className="flex">
+            <div>
+                <img 
+                    src="https://www.hss.edu/images/articles/acl-tear.jpg"
+                    alt="bones"
+                    className="w-[1000px] h-auto"
+                />
+                {/* Temporyary Blue, just to show the boundary
+                    TODO: rounded border
+                    TODO: overlap with the bone pic (prolly z-axis)
+                */}
+                <div className="bg-blue-500 h-[250px] flex justify-center">
+                    Deep Clinical Search
+                </div>
+            </div>
+
+            {/* Temporyary Red, just to show the boundary */}
+            <div className="bg-red-500 w-[1000px] flex justify-center">
+                <div className="pt-5">Explain My Scan</div>
+            
+                
+                
+            </div>
+        </div>
+    );
 }
 
-export default Analysis
+export default Analysis;
