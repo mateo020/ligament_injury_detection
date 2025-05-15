40f84f5cd8f9c131fdca2361ea5dffc22bfa859f
Nicholas Cammisuli <nicholas.cammisuli@gmail.com>
2025-05-14 20:13:32 -0400

    changed bg color and rearranged the layout

--- mri/src/components/Analysis.tsx
+++ mri/src/components/Analysis.tsx
@@ -1,40 +1,41 @@
-import { useEffect } from 'react';
+// import { useEffect } from 'react';
 
 function Analysis() {
-  useEffect(() => {
-    // Disable scrolling
-    document.body.style.overflow = 'hidden';
+//   useEffect(() => {
+//     // Disable scrolling
+//     document.body.style.overflow = 'hidden';
 
-    // Optional cleanup on unmount
-    return () => {
-      document.body.style.overflow = 'auto';
-    };
-  }, []);
-  
+//     // Optional cleanup on unmount
+//     return () => {
+//       document.body.style.overflow = 'auto';
+//     };
+//   }, []);
+
     return (
-
-        <div className="flex ">
-            <div className="relative">
-                <img 
-                    // change this image to uploaded image
-                    src="https://www.hss.edu/images/articles/acl-tear.jpg"
-                    alt="bones"
-                    className="w-[3000px] h-[900px]"
-                />
-                {/* Temporyary Blue, just to show the boundary
-                    TODO: overlap with the bone pic (prolly z-axis)
-                */}
-                <div className="absolute bottom-0 left-7 bg-white/75 h-[250px] w-2xl flex justify-center rounded-t-2xl z-40">
-                    <div className="pt-5">Deep Clinical Search</div>
-                </div>
+        <div>
+            <div className="absolute top-4 left-8 font-bold text-4xl z-50 m-0 p-0">
+                ACL Ligament Helper
             </div>
 
-            {/* Temporyary Red, just to show the boundary */}
-            <div className="bg-red-500 w-[2000px] h-auto flex justify-center">
-                <div className="pt-5">Explain My Scan</div>
-            
+            <div className="flex mt-15">
                 
-                
+                {/* TODO make the divs wider and a 50/50 size split */}
+                <div className="relative">
+                    <img 
+                        // change this image to uploaded image
+                        src="https://www.hss.edu/images/articles/acl-tear.jpg"
+                        alt="bones"
+                        className="w-[3000px] h-[900px]"
+                    />
+                    
+                    <div className="bg-white/75 h-[250px] w-2xl flex justify-center rounded-t-2xl">
+                        <div className="pt-5">Deep Clinical Search</div>
+                    </div>
+                </div>
+
+                <div className="bg-gray-100 w-[2000px] h-auto flex justify-center">
+                    <div className="pt-5">Explain My Scan</div> 
+                </div>
             </div>
         </div>
     );
