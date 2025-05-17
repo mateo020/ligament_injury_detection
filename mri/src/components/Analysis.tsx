6486a221adb68554d920092da4f9ecfdff733ce2
Nicholas Cammisuli <nicholas.cammisuli@gmail.com>
2025-05-17 13:18:30 -0400

    Changed layout for analysis page

--- mri/src/components/Analysis.tsx
+++ mri/src/components/Analysis.tsx
@@ -12,32 +12,36 @@ function Analysis() {
 //   }, []);
 
     return (
-        <div>
-            <div className="absolute top-4 left-8 font-bold text-4xl z-50 m-0 p-0">
+        <>
+            <header className="absolute top-4 left-8 font-bold text-4xl z-50 m-0 p-0">
                 ACL Ligament Helper
-            </div>
+            </header>
 
-            <div className="flex mt-15">
+            <main className="w-screen flex">
                 
-                {/* TODO make the divs wider and a 50/50 size split */}
-                <div className="relative">
+                {/* TODO make the divs wider prolly shorter too and a 50/50 size split - just make it like the new image */}
+                <section className=" bg-gray-50 h-9/10 w-1/2 flex justify-center rounded-2xl absolute top-20 left-20">
+                    {/* IDK why this div bellow makes everything nicely aligned */}
+                    <div>
+                    
+                    <div className="pt-5">Deep Clinical Search</div>
+                   
                     <img 
-                        // change this image to uploaded image
+                        // TODO: change this image to uploaded image
                         src="https://www.hss.edu/images/articles/acl-tear.jpg"
                         alt="bones"
-                        className="w-[3000px] h-[900px]"
-                    />
+                        className=" pt-15"
+                    /> 
                     
-                    <div className="bg-white/75 h-[250px] w-2xl flex justify-center rounded-t-2xl">
-                        <div className="pt-5">Deep Clinical Search</div>
                     </div>
-                </div>
 
-                <div className="bg-gray-100 w-[2000px] h-auto flex justify-center">
+                </section>
+
+                <aside className="bg-gray-50 h-9/10 w-4/10 absolute top-20 left-262 justify-center rounded-2xl ml-5">
                     <div className="pt-5">Explain My Scan</div> 
-                </div>
-            </div>
-        </div>
+                </aside>
+            </main>
+        </>
     );
 }
 
