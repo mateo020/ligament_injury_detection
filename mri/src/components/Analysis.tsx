9f053d942dd18ada7be5fedb592eb2059de0dc60
Nicholas Cammisuli <nicholas.cammisuli@gmail.com>
2025-05-17 23:20:48 -0400

    Update Analysis.tsx
    added the button ui

--- mri/src/components/Analysis.tsx
+++ mri/src/components/Analysis.tsx
@@ -20,19 +20,27 @@ function Analysis() {
             <main className="w-screen flex">
                 <section className=" bg-gray-50 h-9/10 w-1/2 flex justify-center rounded-2xl absolute top-20 left-20">
                     {/* IDK why this div bellow makes everything nicely aligned */}
-                    <div>
+                    <div className="flex flex-col items-center">
                     
-                    {/* TODO : change this to a little icon */}
-                    <div className="pt-5">Image</div>
+                        {/* TODO : change this to a little icon */}
+                        <div className="pt-5">Image</div>
 
-                    {/* TODO : add the buttons on the bottom and the labels on the left (ask mateo if wewill have the labels) */}
-                   
-                    <img 
-                        // TODO: change this image to uploaded image
-                        src="https://www.hss.edu/images/articles/acl-tear.jpg"
-                        alt="bones"
-                        className="pt-5 h-9/10 w-7/10 absolute right-8"
-                    /> 
+                        {/* TODO : add the labels on the left  say acl tear/injury */}
+                    
+                        <img 
+                            // TODO: change this image to uploaded image
+                            src="https://www.hss.edu/images/articles/acl-tear.jpg"
+                            alt="bones"
+                            className="pt-12 h-[85%] w-7/10 absolute right-12"
+                        /> 
+
+                        <div className="absolute bottom-4 grid grid-cols-2 gap-2 w-150">
+                            <button className="">Upload X-Ray</button>
+                            <button className="">Upload DICOM</button>
+                            <button className="">Clear Chat</button>
+                            <button className="">New Thread</button>
+                        </div>
+                    
                     
                     </div>
 
